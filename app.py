8d7ab4c1e2f85c1c9a39ad90ca2efffeecbd4350
Retr0 <sebasmorandez@hotmail.com>
2022-04-12 17:44:42 -0500

    finished main code

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, request
-
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
+
+english_bot = ChatBot("Chatterbot",
+storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english")
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/get')
+def getBotResponse():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
+
+if __name__ == "__main__":
+    app.run()
